8ded63be2c456db059dc59e129833e493b00de4e
Darren Butler <Darren-B312@Outlook.com>
2020-02-20 12:29:09 +0000

    basic player movement implementation

---
+++ Assets/__Scripts/Player/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] private float speed = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vMovement = Input.GetAxis("Vertical");
+        float hMovement = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(hMovement * speed, vMovement * speed);
+    }
+}
