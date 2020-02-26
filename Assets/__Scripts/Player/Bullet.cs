ccc91616e336604c679c8cd16fa434abb89694f6
Darren Butler <57804177+Darren-B312@users.noreply.github.com>
2020-02-26 18:17:19 +0000

    basic enemy & collision implementation

--- Assets/__Scripts/Player/Bullet.cs
+++ Assets/__Scripts/Player/Bullet.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(PolygonCollider2D))]
 public class Bullet : MonoBehaviour
 {
     // this class is used only to tag objects as type Bullet
-    void OnBecameInvisible()
-    {
-        Destroy(gameObject); // remove bullet from hierarchy when it leaves the camera view
-    }
+    //void OnBecameInvisible()
+    //{
+    //    Destroy(gameObject); // remove bullet from hierarchy when it leaves the camera view
+    //}
 }
